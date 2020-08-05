77def8baff3996234d11fdfcd40d4fb37df25143
Tony <waiting33118@gmail.com>
2020-08-05 15:32:51 +0800

    feat: add trace drone path feature

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 (function () {
-  mapboxgl.accessToken = 'pk.eyJ1Ijoid2FpdGluZzMzMTE4IiwiYSI6ImNrZDVlZWp6MjFxcXQyeHF2bW0xenU4YXoifQ.iGfojLdouAjsovJuRxjYVA'
+  const token = 'pk.eyJ1Ijoid2FpdGluZzMzMTE4IiwiYSI6ImNrZDVlZWp6MjFxcXQyeHF2bW0xenU4YXoifQ.iGfojLdouAjsovJuRxjYVA'
+  const dataURL = `https://api.mapbox.com/datasets/v1/waiting33118/ckdgqzwtr010x23miea77hh4h/features?access_token=${token}`
+  mapboxgl.accessToken = token
   const map = new mapboxgl.Map({
     style: 'mapbox://styles/waiting33118/ckdfkx3t10k9w1irkp8anuy39',
     center: { lon: 121.533907, lat: 25.043163 },
@@ -55,14 +57,14 @@
       labelLayerId
     )
   })
-  // 縮放
+  // 縮放控制
   const nav = new mapboxgl.NavigationControl({
     showCompass: true,
     showZoom: true,
     visualizePitch: true
   })
   map.addControl(nav, 'bottom-right')
-
+  // 定位控制
   const mbControl = document.querySelector('.mapboxgl-ctrl-bottom-right')
   const btn = document.createElement('button')
   btn.className = 'locate'
@@ -80,6 +82,45 @@
           essential: true
         })
       })
-      .catch(err => console.log(err))
+      .catch(err => console.warn(err))
+  })
+  // 路徑demo
+  const demo1 = document.querySelector('.demo1')
+
+  demo1.addEventListener('click', () => {
+    axios.get(dataURL)
+      .then(api => {
+        const coordinates = api.data.features[0].geometry.coordinates
+        api.data.features[0].geometry.coordinates = [coordinates[0]]
+        if (!map.getSource('trace')) {
+          map.addSource('trace', { type: 'geojson', data: api.data })
+          map.addLayer({
+            id: 'trace',
+            type: 'line',
+            source: 'trace',
+            paint: {
+              'line-color': 'yellow',
+              'line-opacity': 0.75,
+              'line-width': 10
+            }
+          })
+        }
+        map.jumpTo({ center: coordinates[0], zoom: 17 })
+        map.setPitch(50)
+        let i = 0
+        const timer = window.setInterval(() => {
+          if (i < coordinates.length) {
+            api.data.features[0].geometry.coordinates.push(
+              coordinates[i]
+            )
+            map.getSource('trace').setData(api.data)
+            map.panTo(coordinates[i])
+            i++
+          } else {
+            window.clearInterval(timer)
+          }
+        }, 300)
+      })
+      .catch(err => console.warn(err))
   })
 })()
